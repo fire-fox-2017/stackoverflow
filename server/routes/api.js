6bbda2acabae8d9c9f1f0d6c69dc75698b43c057
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-11 17:06:16 +0700

    refactoring routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,17 +3,29 @@ const router = express.Router();
 const user = require('../controllers/user');
 const post = require('../controllers/post');
 const passport = require('passport')
+const auth = require('../helpers/verify');
 
-// user routes
-router.get('/user', user.getAll);
+// USER ROUTES
+router.get('/user', auth.isLogin, user.getAll);
 router.post('/user/signup', user.signup);
-router.put('/user/edit/:id', user.edit);
-router.delete('/user/delete/:id', user.delete);
+router.put('/user/edit/:id', auth.isLogin, user.edit);
+router.delete('/user/delete/:id', auth.isLogin, user.delete);
 router.post('/user/signin', passport.authenticate('local', {session: false}), (req, res) => {
   res.send(req.user);
 });
 
-// post routes
-router.post('/post/create/:userId', post.create);
+/* POST ROUTES */
+
+// get question
+router.get('/question', auth.isLogin, post.getAll);
+
+// post new question
+router.post('/question/create/:userId', auth.isLogin, post.createQuestion);
+
+// post new answer
+router.post('/answer/create/:postId', auth.isLogin, post.createAnswer);
+
+// delete question
+router.delete('/question/delete/:postId', auth.isLogin, post.delete);
 
 module.exports = router;

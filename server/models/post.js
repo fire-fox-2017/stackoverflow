bfc7be8f669fdad6b26d2c7e71beeef0a8f5398b
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-11 17:05:42 +0700

    fixing post and vote models(semoga fix)

--- server/models/post.js
+++ server/models/post.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+let answerSchema = new Schema({
+  userId: {type: Schema.Types.ObjectId, ref: 'User'},
+  content: String,
+  votes: [{
+    userId: {type: Schema.Types.ObjectId, ref: 'User'},
+    count: {type: Number, enum: [1,-1]}
+  }]
+})
+// post is question
 let postSchema = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User'},
   title: String,
   content: String,
-  postType:{type:String, enum: ['q','a']},
-  votes: [{type: Schema.Types.ObjectId, ref: 'Vote'}]
+  votes: [{
+    userId: {type: Schema.Types.ObjectId, ref: 'User'},
+    count: {type: Number, enum: [1,-1]}
+  }],
+  answers: [answerSchema] 
 })
 
 const Post = mongoose.model('Post', postSchema);
--- server/models/vote.js
+++ server/models/vote.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let voteSchema = new Schema({
-  userId: {type: Schema.Types.ObjectId, ref: 'User'},
+  userId: {type: String, ref: 'User'},
   count : {type: Number, enum: [1, -1]}
 })
 

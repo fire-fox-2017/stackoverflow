8aaa2fd1a2485213432c91d5e402f8147541506d
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-14 23:01:17 +0700

    login and register form
    adding register and login components

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,13 +1,19 @@
 <template>
- <div id="login-form" class="ui middle aligned center aligned grid">
+  <div id="login-form" class="ui middle aligned center aligned grid">
   <div class="column">
     <h2 class="ui green image header">
       <div class="content">
-        Log-in to your account
+        Register new account
       </div>
     </h2>
     <form class="ui large form">
       <div class="ui stacked segment">
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="bold icon"></i>
+            <input type="text" v-model="name" name="name" placeholder="Name">
+          </div>
+        </div>
         <div class="field">
           <div class="ui left icon input">
             <i class="user icon"></i>
@@ -20,20 +26,15 @@
             <input type="password" v-model="password" name="password" placeholder="Password">
           </div>
         </div>
-        <div @click="login()" class="ui fluid large green submit button">Login</div>
+        <div @click="onRegister()" class="ui fluid large green submit button">Register</div>
       </div>
 
       <div class="ui error message"></div>
 
     </form>
-
     <div class="ui message">
-      Belum punya akun? <a href="#/register">Daftar Dulu</a>
-      <div class="ui">
-        <a href="#/">Atau kembali ke halaman utama</a>
-      </div>
+      Atau kembali ke login? <a href="#/login">Login Dulu</a>
     </div>
-
   </div>
 </div>
 </template>
@@ -41,33 +42,25 @@
 <script>
 export default {
 
-  name: 'Login',
+  name: 'Register',
 
   data () {
     return {
+      name: '',
       username: '',
-      password: ''
+      password: '',
     };
   },
   methods: {
-    login() {
+    onRegister: function() {
       let self = this;
-      axios.post('http://localhost:3000/api/user/signin', {
+      axios.post('http://localhost:3000/api/user/signup', {
+        name: self.name,
         username: self.username,
         password: self.password
-      }).then((res) => {
-        console.log(res.data)
-        localStorage.setItem('token', res.data.token);
-        localStorage.setItem('user', self.username);
-        localStorage.setItem('userId', res.data.userId);
-        localStorage.setItem('name', res.data.name);
-        alert('login sukses');
-        self.$router.push({ path: '/main' });
-        // window.location.href = '#/main'
-        window.location.reload();
-      }).catch((err) => {
-        console.log(err);
-        alert('login failed');
+      }).then(res => {
+        alert('you have successfully register new user');
+        self.$router.push({ path: '/login' });
       })
     }
   }
@@ -75,9 +68,8 @@ export default {
 </script>
 
 <style lang="css" scoped>
-  #login-form {
+#login-form {
     width: 50%;
     margin: 0 auto;
   }
-    
 </style>

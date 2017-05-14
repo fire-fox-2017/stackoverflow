5197c9ee1654d71187f65614ce624e391cc540d6
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-14 23:02:05 +0700

    adding user name in localstorage and vuex state

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -2,7 +2,8 @@ export const state = {
   userToken: localStorage.getItem('token') || null,
   isLogin: false,
   loggedUser: localStorage.getItem('user') || null,
-  loggedUserId: localStorage.getItem('userId') || null
+  loggedUserId: localStorage.getItem('userId') || null,
+  name: localStorage.getItem('name') || null
 }
 
 export const mutations = {
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,7 +13,7 @@ methods.signup = (req, res, next) => {
     name: name,
     username: username,
     password: password,
-    role: role
+    role: 'User'
   }, (err, user) => {
     if(err) {
       res.json({error: err, success: false});
@@ -30,7 +30,7 @@ methods.signin = (username, password, next) => {
     }
       else if(pwh.verify(password, user.password)) {
         let userData = Object.assign({}, user.toJSON());
-        next(null, {message: 'login sukses', token: verify.auth(userData), success: true, username: user.username, userId: user._id});
+        next(null, {message: 'login sukses', token: verify.auth(userData), success: true, username: user.username, userId: user._id, name: user.name});
         console.log(userData);
       } else {
         next(null, {message: 'password anda salah', success: false});
@@ -68,7 +68,7 @@ methods.edit = (req, res, next) => {
       user.username = req.body.username ? req.body.username : user.username;
       user.password = req.body.password ? pwh.generate(req.body.password) : user.password;
       user.role = req.body.role ? req.body.role : user.role;
-      // user.save();
+      user.save();
       res.json({user: user, success: true});
     }
   })
